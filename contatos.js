c44947bcca12a0d4ab5589b04a0750da5daa8fa1
nanzim2 <renan.andrade2006@gmail.com>
2025-04-25 23:21:25 -0300

    Limpeza codigo-8

--- contatos.js
+++ contatos.js
@@ -12,3 +12,16 @@ document.getElementById('feedbackForm').addEventListener('submit', function(e) {
 
     window.open(url, '_blank');
   });
+
+const modo = document.getElementById('botao-tema')
+
+  modo.addEventListener('change', () => {
+    document.body.classList.toggle('dark')
+})
+
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav');
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
