bffa41c0c265c9cb41be608082f9973531d25721
nanzim2 <renan.andrade2006@gmail.com>
2025-04-27 17:06:51 -0300

    Limpeza contatos

--- contatos.js
+++ contatos.js
@@ -13,15 +13,24 @@ document.getElementById('feedbackForm').addEventListener('submit', function(e) {
     window.open(url, '_blank');
   });
 
-const modo = document.getElementById('botao-tema')
+const modo = document.getElementById('botao-tema') //modo claro e escuro
 
   modo.addEventListener('change', () => {
     document.body.classList.toggle('dark')
 })
 
-  const hamburger = document.querySelector('.hamburger');
+  const hamburger = document.querySelector('.hamburger'); // NAV-BAR
   const nav = document.querySelector('.nav');
 
   hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
+});
+
+const textarea = document.getElementById('mensagem'); //Inicio contador
+const contador = document.getElementById('contador');
+
+textarea.addEventListener('input', function() {
+  const caracteresDigitados = textarea.value.length;
+  const caracteresMax = textarea.getAttribute('maxlength');
+  contador.textContent = `${caracteresDigitados}/${caracteresMax}`;
 });
